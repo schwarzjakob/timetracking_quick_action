6862bc0a99e2bc69758ddc8cbc8d32190bbc68b6
Jakob Schwarz <jakob.schwarz97@gmail.com>
2024-03-31 16:45:00 +0200

    Change canvas variable name

--- script.py
+++ script.py
@@ -19,20 +19,20 @@ def generate_pdf(
     project_name,
     project_reference,
 ):
-    c = canvas.Canvas(output_filename, pagesize=letter)
+    pdf_canvas = canvas.Canvas(output_filename, pagesize=letter)
 
     # Title with current month and year
     title = "Stundennachweis " + invoice_month_year
-    c.setFont("Helvetica-Bold", 12)
-    c.drawString(30, letter[1] - 30, title)
+    pdf_canvas.setFont("Helvetica-Bold", 12)
+    pdf_canvas.drawString(30, letter[1] - 30, title)
 
-    c.setFont("Helvetica", 10)
-    c.drawString(30, letter[1] - 50, "Auftraggeber: ")
-    c.drawString(150, letter[1] - 50, client_name)
-    c.drawString(30, letter[1] - 70, "project:")
-    c.drawString(150, letter[1] - 70, project_name)
-    c.drawString(30, letter[1] - 90, "projectreferenz:")
-    c.drawString(150, letter[1] - 90, project_reference)
+    pdf_canvas.setFont("Helvetica", 10)
+    pdf_canvas.drawString(30, letter[1] - 50, "Auftraggeber: ")
+    pdf_canvas.drawString(150, letter[1] - 50, client_name)
+    pdf_canvas.drawString(30, letter[1] - 70, "project:")
+    pdf_canvas.drawString(150, letter[1] - 70, project_name)
+    pdf_canvas.drawString(30, letter[1] - 90, "projectreferenz:")
+    pdf_canvas.drawString(150, letter[1] - 90, project_reference)
 
     # Table headers
     headers = ["Datum", "Anzahl Stunden", "Aufgabe"]
@@ -40,18 +40,18 @@ def generate_pdf(
         letter[1] - 130
     )  # Start position for printing rows after additional information
     for header in headers:
-        c.drawString(30 + headers.index(header) * 150, y_position, header)
+        pdf_canvas.drawString(30 + headers.index(header) * 150, y_position, header)
 
     # Table rows
     y_position -= 20
     for index, row in dataframe.iterrows():
         y_position -= 20
         date = format_date(row["start"])  # Format Datum
-        c.drawString(30, y_position, date)  # Datum
-        c.drawString(180, y_position, str(row["duration"]))  # Anzahl Stunden
-        c.drawString(330, y_position, row["task"])  # Aufgabe
+        pdf_canvas.drawString(30, y_position, date)  # Datum
+        pdf_canvas.drawString(180, y_position, str(row["duration"]))  # Anzahl Stunden
+        pdf_canvas.drawString(330, y_position, row["task"])  # Aufgabe
 
-    c.save()
+    pdf_canvas.save()
 
 
 # Assuming the first argument is the CSV file path
