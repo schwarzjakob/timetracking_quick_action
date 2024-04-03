ad827beea23094a237b3ad39285bfd9cf0c4d8ec
Jakob Schwarz <jakob.schwarz97@gmail.com>
2024-04-03 13:13:10 +0200

    Update script with jwm design and better bash workflow via automator

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import os
 import re
 
 # Import necessary modules from reportlab
-from reportlab.lib.pagesizes import letter
+from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen.canvas import Canvas
 from PyPDF2 import PdfReader, PdfWriter
 from reportlab.platypus import SimpleDocTemplate, Spacer, Paragraph, Table, TableStyle
@@ -13,7 +13,18 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib import colors
 from reportlab.lib.enums import TA_CENTER, TA_LEFT
 from reportlab.lib.colors import black
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 
+# Register the font (only needs to be done once)
+font_path_bold = sys.argv[2] + "/font-bold.ttf"
+font_name_bold = "font-bold"
+
+font_path_medium = sys.argv[2] + "/font-medium.ttf"
+font_name_medium = "font-medium"
+
+pdfmetrics.registerFont(TTFont(font_name_bold, font_path_bold))
+pdfmetrics.registerFont(TTFont(font_name_medium, font_path_medium))
 
 # Define custom styles
 custom_styles = {
@@ -23,14 +34,14 @@ custom_styles = {
         leading=22,
         alignment=TA_LEFT,
         spaceAfter=20,
-        fontName="Helvetica-Bold",  # Ensure title is bold
+        fontName=font_name_bold,  # Ensure title is bold
     ),
     "body": ParagraphStyle(
         name="Body",
         fontSize=12,
         leading=14,
         spaceAfter=6,
-        fontName="Helvetica",  # Normal text, not bold
+        fontName=font_name_medium,  # Normal text, not bold
     ),
     "table_heading": ParagraphStyle(
         name="TableHeading",
@@ -38,7 +49,7 @@ custom_styles = {
         leading=12,
         alignment=TA_LEFT,
         textColor=black,
-        fontName="Helvetica-Bold",  # Ensure table headings are bold
+        fontName=font_name_bold,  # Ensure table headings are bold
         spaceAfter=6,
     ),
     "table_body": ParagraphStyle(
@@ -47,8 +58,8 @@ custom_styles = {
         leading=12,
         alignment=TA_LEFT,
         textColor=black,
-        fontName="Helvetica",  # Normal text, not bold for table body
-    )
+        fontName=font_name_medium,  # Normal text, not bold for table body
+    ),
 }
 
 
@@ -103,7 +114,7 @@ def format_date(date_string):
 def add_pdf_background(pdf_path, background_pdf_path):
     """
     Adds a PDF background to the input PDF file.
-    
+
     Args:
     - input_pdf_path: Path to the generated PDF invoice.
     - background_pdf_path: Path to the PDF file to use as background.
@@ -140,7 +151,22 @@ def generate_project_invoice_as_pdf(
     project_reference,
 ):
     """ " Generates a PDF invoice for a project based on the provided DataFrame."""
-    doc = SimpleDocTemplate(output_directory_and_filename, pagesize=letter)
+
+    # Define the page size and margins
+    page_width, page_height = A4
+    top_bottom_margin = 72  # 2.54 cm in points
+    left_right_margin = 54  # 1.91 cm in points
+    print(page_width, page_height, top_bottom_margin, left_right_margin)
+
+    # Create a document with margins
+    doc = SimpleDocTemplate(
+        output_directory_and_filename,
+        pagesize=A4,
+        rightMargin=left_right_margin,
+        leftMargin=left_right_margin,
+        topMargin=top_bottom_margin,
+        bottomMargin=top_bottom_margin,
+    )
     story = []
     styles = getSampleStyleSheet()
 
@@ -150,31 +176,57 @@ def generate_project_invoice_as_pdf(
     )
 
     # Add client and project info
-    story.append(Spacer(1, 12))  # Add some space
-    story.append(Paragraph("Auftraggeber: " + client_name, custom_styles["body"]))
-    story.append(Paragraph("Projekt: " + project_name, custom_styles["body"]))
-    story.append(
-        Paragraph("Projektreferenz: " + project_reference, custom_styles["body"])
+    # Prepare data for the table
+    client_project_data = [
+        ["Auftraggeber: ", client_name],
+        ["Projekt: ", project_name],
+        ["Projektreferenz: ", project_reference],
+    ]
+
+    # Create the table
+    client_project_table = Table(
+        client_project_data, colWidths=[100, page_width - 2 * left_right_margin - 100]
+    )
+    client_project_table.setStyle(
+        TableStyle(
+            [
+                ("ALIGN", (0, 0), (-1, -1), "LEFT"),
+                ("FONTNAME", (0, 0), (-1, -1), font_name_medium),
+                ("FONTNAME", (0, 1), (-1, -1), font_name_medium),
+            ]
+        )
     )
 
-    story.append(Spacer(1, 20))  # Space before the table
+    story.append(client_project_table)
+
+    # Space before the table
+    story.append(Spacer(1, 20))
 
     # Prepare data for the table including header
     data = [["Datum", "Anzahl Stunden", "Aufgabe"]] + [
         [format_date(row["start"]), row["duration"], row["task"]]
         for _, row in dataframe.iterrows()
     ]
+    total_duration = dataframe["duration"].sum()
+    data.append(["Summe ", total_duration, ""])
 
-    # Create table
-    column_widths = [68, 100, 300]
-    table = Table(data, colWidths=column_widths)
-    table.setStyle(
+    # Calculate the width of the page minus the margins and the column widths
+    content_width = page_width - 2 * left_right_margin
+    col1_width = 100
+    col2_width = 100
+    col3_width = content_width - col1_width - col2_width
+    print(content_width, col1_width, col2_width, col3_width)
+
+    # Create the table
+    column_widths = [col1_width, col2_width, col3_width]
+    data_table = Table(data, colWidths=column_widths)
+    data_table.setStyle(
         TableStyle(
             [
                 ("ALIGN", (0, 0), (-1, -1), "LEFT"),
                 ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
                 ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
-                ("GRID", (0, 0), (-1, -1), 1, colors.black),
+                ("GRID", (0, 0), (-1, -1), 0.5, colors.black),
                 # Apply custom styles to table headings and body
                 (
                     "TEXTCOLOR",
@@ -185,18 +237,24 @@ def generate_project_invoice_as_pdf(
                 ("FONTNAME", (0, 0), (-1, 0), custom_styles["table_heading"].fontName),
                 ("TEXTCOLOR", (0, 1), (-1, -1), custom_styles["table_body"].textColor),
                 ("FONTNAME", (0, 1), (-1, -1), custom_styles["table_body"].fontName),
+                (
+                    "FONTNAME",
+                    (0, -1),
+                    (-1, -1),
+                    custom_styles["table_heading"].fontName,
+                ),  # Make last row bold
             ]
         )
     )
 
-    story.append(table)
+    story.append(data_table)
     doc.build(story)
 
 
 if __name__ == "__main__":
     # Assuming the first argument is the CSV file path
     csv_file = sys.argv[1]
-    pdf_background = sys.argv[2]
+    pdf_background = sys.argv[2] + "/background.pdf"
 
     # Extract directory path from the CSV file path and define output directory
     current_directory = os.path.dirname(csv_file)
